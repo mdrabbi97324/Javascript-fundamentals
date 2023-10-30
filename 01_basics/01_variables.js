a83a414258564d358a524f4098f702a73fc73ab9
Md. Rabbi <133827787+mdrabbi97324@users.noreply.github.com>
2023-10-30 04:00:55 +0000

    variable usecases

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+/*
+1. Constant variable are not changable 
+2. let declare variable are changable
+3. var declare variable are changable. It's not preferable
+    to use var because if we declare a variable same name 
+    using var keyword it will change previous all var declare variables.
+4.Don't decalare a variable without using const, let or var keyword ;
+5. If you declare a variable without assinging a value it will show undefined;
+ */
+const accountId = 140;
+let accountEmail = 'golamrabbi@google.com';
+var accountPassword = '123456';
+accountCity = 'Dhaka';
+
+let accountState;
+
+console.log(accountId);
+// accountId = 144; // It's not allowed to change a const variable
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+accountEmail = 'gr@yahoo.com'
+accountPassword = '2222'
+accountCity = 'Rajshahi'
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+

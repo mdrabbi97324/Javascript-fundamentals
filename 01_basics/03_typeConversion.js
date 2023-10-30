537bf5a556b17243b67d96125ede17a8bdb96c79
Md. Rabbi <133827787+mdrabbi97324@users.noreply.github.com>
2023-10-30 05:47:43 +0000

    type conversion

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,40 @@
+let score = "";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let scoreInNumber = Number(score)
+// console.log(scoreInNumber)
+// console.log(typeof scoreInNumber)
+
+let name = "";
+// console.log(typeof name);
+
+let nameInBoolean = Boolean(name);
+// console.log(nameInBoolean);
+
+let isLoggedIn = true;
+let isLoggedInNumber = Number(isLoggedIn)
+console.log(isLoggedInNumber);
+
+/* 
+Boolean to number 
+true => 1
+false => 0
+null => 0
+
+*/
+
+
+
+// String to boolean 
+// "hello" => true;
+// "" => false
+// " " => true
+// 
+
+
+// string to number 
+
+// 1. "" => 0;
+// 2. "a" => NaN;
+// 3. "24" = 24

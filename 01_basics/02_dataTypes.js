b8d2f420ad591108c24435a2a5064cb6458a9ddf
Md. Rabbi <133827787+mdrabbi97324@users.noreply.github.com>
2023-10-30 04:34:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+/* 
+1. use strict keyword treat all JS code as newer version
+2. we are node JS not browser that's why alert shows not defined
+3.Code readability should be high 
+4. JS typeof null is an object 
+5. JS typeof undefined is undefined
+*/
+"use strict";
+
+// alert(3+3) 
+
+console.log(3
+    + 3);
+console.log('Golam Rabbi');
+
+
+// Some DataTypes are here
+let name = 'Golam'
+let age = 99
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => which means it's value is not defined;
+// symbol => use for unuiqe identity
+
+
+// object 
+
+console.log(typeof "Go language"); // string
+console.log(typeof null) // object
+console.log(typeof undefined)

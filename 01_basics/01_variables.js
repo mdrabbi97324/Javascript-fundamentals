d08d4cc079a556ff664ebde1ea0036a2bbbe83bb
Md. Rabbi <133827787+mdrabbi97324@users.noreply.github.com>
2023-10-30 04:07:23 +0000

    #

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,7 @@
 3. var declare variable are changable. It's not preferable
     to use var because if we declare a variable same name 
     using var keyword it will change previous all var declare variables.
+    It also create issues in block scope and funtional scope.
 4.Don't decalare a variable without using const, let or var keyword ;
 5. If you declare a variable without assinging a value it will show undefined;
  */

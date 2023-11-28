d5e859e244077e894ccdae2447cdb2c59945457d
Md. Rabbi <133827787+mdrabbi97324@users.noreply.github.com>
2023-11-28 02:42:09 +0000

    scope and basic closure

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -10,7 +10,54 @@ if(true) {
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const username = "Golam Rabbi"
+    function two(){
+        const website = "Youtube";
+        console.log(username)
+    }
+    two()
+    // console.log(website); // website is not defined
+}
+
+// one()
+
+if(true){
+    const username = "Golam"
+    if(username === "Golam"){
+        console.log(username)
+        const website = " Youtube"
+    }
+
+    // console.log(username + website) // website is not defined
+}
+
+// console.log(username)  // username is not defined
+
+// ++++++++++++++Interesting++++++++++++
+
+
+
+console.log(addOne(10)); // 11
+function addOne(num){
+    return num + 1;
+}
+
+
+
+console.log(addTwo(10)) // cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2;
+}
+
+
+
+
+
+
+
 
